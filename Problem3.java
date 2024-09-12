a72def581c0a32d08aff679db23642b853c86d0f
shrek-wth-no-r <cmsarr100@student.hbuhsd.edu>
2024-09-12 11:20:59 -0700

    sdsd

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,17 @@
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    double num1 = 53.329;
+    double num2;
+    double num3;
+    num1 = (num1 %= 10)/10;
+    num2 = (num1 %= 10)/100;
+    num3 = (num1 %= 10)/1000;
+
+
+    System.out.println(num1 + " " + num2 + " " + num3);
+  }
+}
+
 
